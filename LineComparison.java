8328d8620e857d134478c2ed35a5197271b4d21d
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-18 16:07:50 +0530

    added equality of line program

--- LineComparison.java
+++ LineComparison.java
@@ -3,32 +3,57 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class LineComparison {
+	
+	public String line1;
+	public String line2;
+	
+	public void equalityOfLine() {
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enetr the x co-ordinate of starting point of line 1:  " );
+	int x1 = input.nextInt();
+	
+	System.out.println("Enetr the y co-ordinate of starting point of line 1 :  " );
+	int y1 = input.nextInt();
+	
+	System.out.println("Enetr the x co-ordinate of ending point of line 1:  " );
+	int x2 = input.nextInt();
+	
+	System.out.println("Enetr the y co-ordinate of ending point of line 1:  " );
+	int y2 = input.nextInt();
+	
+	System.out.println("Enetr the x co-ordinate of starting point of line 2:  " );
+	int x_1 = input.nextInt();
+	
+	System.out.println("Enetr the y co-ordinate of starting point of line 2 :  " );
+	int y_1 = input.nextInt();
+	
+	System.out.println("Enetr the x co-ordinate of ending point of line 2:  " );
+	int x_2 = input.nextInt(); 
+	
+	System.out.println("Enetr the y co-ordinate of ending point of line 2:  " );
+	int y_2 = input.nextInt();
+	
+	int lengthOfline1 = ( (x2 - x1) ^ 2 + (y2 -y1)^2)^(1/2);
+	
+	String line1 = Integer.toString(lengthOfline1); 
+	
+	int lengthOfline2 = ( (x_2 - x_1) ^ 2 + (y_2 -y_1)^2)^(1/2);
+
+	String line2 = Integer.toString(lengthOfline2);
+	
+	System.out.println(line1.equals(line2));
+	
+	input.close();
+	}
+
 
 	public static void main(String[] args) {
 		LineComparison line = new LineComparison();
-		line.length();
-		
-	}
-	
-	public void length() {
-//		taking input from user for co-ordinates
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enetr the x co-ordinate of starting point :  " );
-		int x1 = input.nextInt();
-		
-		System.out.println("Enetr the y co-ordinate of starting point :  " );
-		int y1 = input.nextInt();
-		
-		System.out.println("Enetr the x co-ordinate of ending point :  " );
-		int x2 = input.nextInt();
-		
-		System.out.println("Enetr the y co-ordinate of starting point :  " );
-		int y2 = input.nextInt();
-			
-//		computing length of line
-			
-		int lengthOfline = ( (x2 - x1) ^ 2 + (y2 -y1)^2)^(1/2);
-		System.out.println("length of line " + " "+ lengthOfline);
-		input.close();	
+		line.equalityOfLine();
 	}
 }
+	
+		
+	
+
